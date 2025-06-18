c0b18a5b21c4796d62cda8e6fdc0494e0743fdef
DegsRed <evanbrokeit@gmail.com>
2025-06-18 15:05:40 -0400

    Added stats

--- main.py
+++ main.py
@@ -1,14 +1,10 @@
+from stats import get_word_count
+
 def get_book_text():
     with open("books/frankenstein.txt") as book:
         book_contents = book.read()
     return book_contents
 
-def get_word_count(text):
-    word_count = 0
-    words = text.split()
-    for word in words:
-        word_count += 1
-    return word_count
 
 def main(): 
     word_count = get_word_count(get_book_text())
---
+++ stats.py
@@ -0,0 +1,9 @@
+def get_word_count(text):
+    word_count = 0
+    words = text.split()
+    for word in words:
+        word_count += 1
+    return word_count
+def get_charcter_amount(text):
+    pass
+
