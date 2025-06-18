633f0b9c7bc62dd13899404ba649c0e9c727c9be
DegsRed <evanbrokeit@gmail.com>
2025-06-18 15:23:38 -0400

    Now lists the amount of all characters

--- main.py
+++ main.py
@@ -1,13 +1,14 @@
 from stats import get_word_count
+from stats import get_character_amount
 
 def get_book_text():
     with open("books/frankenstein.txt") as book:
         book_contents = book.read()
     return book_contents
 
-
 def main(): 
     word_count = get_word_count(get_book_text())
     print(f"{word_count} words found in the document")
+    print(get_character_amount(get_book_text()))
 
 main()
--- stats.py
+++ stats.py
@@ -4,6 +4,12 @@ def get_word_count(text):
     for word in words:
         word_count += 1
     return word_count
-def get_charcter_amount(text):
-    pass
-
+def get_character_amount(text):
+    lowercase_text = text.lower()
+    character_list = {}
+    for char in lowercase_text:
+        if char in character_list:
+            character_list[char] += 1
+        else:
+            character_list[char] = 1
+    return character_list
