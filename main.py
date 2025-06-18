310f0e0e0c12870768def6bf023e15371dadc8f1
DegsRed <evanbrokeit@gmail.com>
2025-06-18 14:14:18 -0400

    main now prints all of Frankenstein

--- main.py
+++ main.py
@@ -1,1 +1,8 @@
-print("greetings boots")
+def get_book_text():
+    with open("books/frankenstein.txt") as book:
+        book_contents = book.read()
+    return book_contents
+def main():
+    all_words = get_book_text()
+    print(all_words)
+main()
